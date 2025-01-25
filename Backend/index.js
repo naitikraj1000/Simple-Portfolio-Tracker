6566480b2df813e0e24747a9a7852a6c6b082c78
Naitik Raj <naitikraj10001@gmail.com>
2025-01-25 23:20:02 +0530

    CORS POLICY FIX

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 dotenv.config();
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:3000','https://simple-portfolio-tracker-u1uvwjvu9-naitikraj1000s-projects.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
