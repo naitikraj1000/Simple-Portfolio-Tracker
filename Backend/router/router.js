26ed533476e2935cb38c22e044ce5312c65152ba
Naitik Raj <naitikraj10001@gmail.com>
2025-01-26 00:01:01 +0530

    CORS FIX

--- Backend/router/router.js
+++ Backend/router/router.js
@@ -2,14 +2,19 @@ import { Router } from "express";
 import getStocks from "../component/stock.js";
 import isauth from "../component/auth.js";
 import { verifyToken } from "../component/auth.js";
+import dotenv from 'dotenv';
 import { signin, logout } from "../component/auth.js";
-import {getuserstocks, signup, addstock, updatestock, deletestock,getuserprotfoliostocks } from "../component/portfolio.js";
+import { getuserstocks, signup, addstock, updatestock, deletestock, getuserprotfoliostocks } from "../component/portfolio.js";
 const router = Router();
 
+dotenv.config();
+console.log(process.env.FRONTEND_URL);
+router.get("/", (req, res) => {
+    res.send(`Backend is Running Fine and Frontend URL is ${process.env.FRONTEND_URL}`);
+});
 
-
-router.get("/getuserstock",getuserstocks);
-router.get("/getuserprotfoliostocks",getuserprotfoliostocks);
+router.get("/getuserstock", getuserstocks);
+router.get("/getuserprotfoliostocks", getuserprotfoliostocks);
 router.post("/signup", signup);
 router.post("/addstock", addstock);
 router.put("/updatestock", updatestock);
